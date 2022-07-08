a2cd12db570e11a2306d7ccab631758fbfcbe099
Kai Ren Chen <69485834+kairenchen721@users.noreply.github.com>
2022-07-08 17:18:21 -0400

    1. add decoy prefix to both protein hit and peptide evidence

--- src/epifany_single_component_maker.py
+++ src/epifany_single_component_maker.py
@@ -1,94 +1,169 @@
 # this code is largely inspired by
 # https://pyopenms.readthedocs.io/en/latest/datastructures_id.html
+import sys
 
 from pyopenms import AASequence, IdXMLFile, PeptideEvidence, PeptideHit, \
     PeptideIdentification, ProteinHit, ProteinIdentification
 
 
 def main(out_file_name):
-
     # for proteins
     protein_id = ProteinIdentification()
     protein_id.setIdentifier("IdentificationRun1")
 
-    target_protein_accession_list = ['PAL4E', 'PAL4H', 'PAL4D', 'PAL4F',
-                                     'PAL4A', 'A0A0H2UH3',
-                                     'PAL4C', 'PAL4G', 'PPIA', 'C9J5S7',
-                                     'F8WE65', 'A0A7I2V5J5',
+    target_protein_accession_list = ['PAL4E', 'PAL4H', 'PAL4D',
+                                     'PAL4F', 'PAL4A', 'A0A0H2UH3',
+                                     'PAL4C', 'PAL4G', 'PPIA',
+                                     'C9J5S7', 'F8WE65', 'A0A7I2V5J5',
                                      'A0A7P0S768', 'E5RIZ5', 'A0A7I2V4V1']
-    decoy_protein_accession_list = target_protein_accession_list.copy()
+    decoy_protein_accession_list = ['DECOY PAL4E', 'DECOY PAL4H', 'DECOY PAL4D',
+                                    'DECOY PAL4F', 'DECOY PAL4A', 'DECOY A0A0H2UH3',
+                                    'DECOY PAL4C', 'DECOY PAL4G', 'DECOY PPIA',
+                                    'DECOY C9J5S7', 'DECOY F8WE65', 'DECOY A0A7I2V5J5',
+                                    'DECOY A0A7P0S768', 'DECOY E5RIZ5', 'DECOY A0A7I2V4V1']
 
     make_protein_hit_list(protein_id, target_protein_accession_list,
                           decoy_protein_accession_list)
 
-
-    # for peptides
-    peptide_id_list = []
-
-    peptide_id = fill_peptide_id('TEWLDGK',
-                    ['PAL4E', 'PAL4H', 'PAL4D', 'PAL4F', 'PAL4A',
-                     'A0A0H2UH3', 'PAL4C', 'PAL4G', 'PPIA'],
-                    1.0852594434272e-05, False)
-    peptide_id_list.append(peptide_id)
-
-    peptide_id = fill_peptide_id('IIPGFMCQGGDFTR',
-                    ['PAL4E', 'PAL4H', 'PAL4D', 'PAL4F', 'PAL4A',
-                     'A0A0H2UH3', 'PAL4C', 'PPIA', 'C9J5S7', 'F8WE65'],
-                    1.0852594434272e-05, False)
-    peptide_id_list.append(peptide_id)
-
-    peptide_id = fill_peptide_id('HTGSGILSMANAGPNTNGSQFFICTAK',
-                    ['PAL4C', 'PAL4G', 'PPIA', 'C9J5S7', 'F8WE65',
-                     'A0A7I2V4V1'],
-                    1.52664048381647e-05, False)
-    peptide_id_list.append(peptide_id)
-
-    peptide_id = fill_peptide_id('ITIADCGQLE',
-                    ['PPIA'],
-                    0.000330313269055314, False)
-    peptide_id_list.append(peptide_id)
-
-    peptide_id = fill_peptide_id('EGMNIVEAMER',
-                    ['PPIA'],
-                    1.52664048381647e-05, False)
-    peptide_id_list.append(peptide_id)
-
-    peptide_id = fill_peptide_id('FEDENFILK',
-                    ['PPIA', 'A0A7I2V4V1', 'C9J5S7', 'F8WE65'],
-                    1.0852594434272e-05, False)
-    peptide_id_list.append(peptide_id)
-
-    peptide_id = fill_peptide_id('VSFELFADKVPK',
-                    ['A0A7I2V5J5', 'A0A7I2V4V1', 'C9J5S7', 'F8WE65'],
-                    1.52664048381647e-05, False)
-    peptide_id_list.append(peptide_id)
-
-    peptide_id = fill_peptide_id('VSFELFADK',
-                    ['A0A7I2V5J5', 'A0A7I2V4V1', 'C9J5S7', 'F8WE65'],
-                    1.0852594434272e-05, False)
-    peptide_id_list.append(peptide_id)
-
-    peptide_id = fill_peptide_id('VNPTVFFDIAVDGEPLGR',
-                    ['PPIA', 'A0A7I2V5J5', 'A0A7P0S768', 'C9J5S7', 'E5RIZ5',
-                     'F8WE65'],
-                    0.000111454024641728, False)
-    peptide_id_list.append(peptide_id)
-
-    peptide_id = fill_peptide_id('CQGGDFTR',
-                    ['A0A7I2V4V1'],
-                    3.12238306472365e-05, False)
-    peptide_id_list.append(peptide_id)
-
-    peptide_id = fill_peptide_id('MCQGGDFTR',
-                    ['A0A7I2V4V1'],
-                    1.52664048381647e-05, False)
-    peptide_id_list.append(peptide_id)
-
+    peptide_id_list = fill_all_peptide_id()
 
     # make protein identification into a list
     protein_id_list = [protein_id]
 
     IdXMLFile().store(out_file_name, protein_id_list, peptide_id_list)
+
+
+def fill_all_peptide_id():
+    # for peptides
+    peptide_id_list = []
+    peptide_id = fill_peptide_id('TEWLDGK',
+                                 ['PAL4E', 'PAL4H', 'PAL4D', 'PAL4F', 'PAL4A',
+                                  'A0A0H2UH3', 'PAL4C', 'PAL4G', 'PPIA'],
+                                 1.0852594434272e-05, False)
+    peptide_id_list.append(peptide_id)
+    peptide_id = fill_peptide_id('IIPGFMCQGGDFTR',
+                                 ['PAL4E', 'PAL4H', 'PAL4D', 'PAL4F', 'PAL4A',
+                                  'A0A0H2UH3', 'PAL4C', 'PPIA', 'C9J5S7',
+                                  'F8WE65'],
+                                 1.0852594434272e-05, False)
+    peptide_id_list.append(peptide_id)
+    peptide_id = fill_peptide_id('HTGSGILSMANAGPNTNGSQFFICTAK',
+                                 ['PAL4C', 'PAL4G'],
+                                 1.52664048381647e-05, False)
+    peptide_id_list.append(peptide_id)
+    peptide_id = fill_peptide_id('HTGPGILSMANAGPNTNGSQFFICTAK',
+                                 ['PPIA', 'C9J5S7', 'F8WE65',
+                                  'A0A7I2V4V1'],
+                                 1.52664048381647e-05, False)
+    peptide_id_list.append(peptide_id)
+    peptide_id = fill_peptide_id('ITIADCGQLE',
+                                 ['PPIA'],
+                                 0.000330313269055314, False)
+    peptide_id_list.append(peptide_id)
+    peptide_id = fill_peptide_id('EGMNIVEAMER',
+                                 ['PPIA'],
+                                 1.52664048381647e-05, False)
+    peptide_id_list.append(peptide_id)
+    peptide_id = fill_peptide_id('FEDENFILK',
+                                 ['PPIA', 'A0A7I2V4V1', 'C9J5S7', 'F8WE65'],
+                                 1.0852594434272e-05, False)
+    peptide_id_list.append(peptide_id)
+    peptide_id = fill_peptide_id('VSFELFADKVPK',
+                                 ['PPIA', 'A0A7I2V5J5', 'A0A7I2V4V1', 'C9J5S7',
+                                  'F8WE65'],
+                                 1.52664048381647e-05, False)
+    peptide_id_list.append(peptide_id)
+    peptide_id = fill_peptide_id('VSFELFADK',
+                                 ['A0A7I2V5J5', 'A0A7I2V4V1', 'C9J5S7',
+                                  'F8WE65'],
+                                 1.0852594434272e-05, False)
+    peptide_id_list.append(peptide_id)
+    peptide_id = fill_peptide_id('VNPTVFFDIAVDGEPLGR',
+                                 ['PPIA', 'A0A7I2V5J5', 'A0A7P0S768', 'C9J5S7',
+                                  'E5RIZ5',
+                                  'F8WE65'],
+                                 0.000111454024641728, False)
+    peptide_id_list.append(peptide_id)
+    peptide_id = fill_peptide_id('CQGGDFTR',
+                                 ['A0A7I2V4V1'],
+                                 3.12238306472365e-05, False)
+    peptide_id_list.append(peptide_id)
+    peptide_id = fill_peptide_id('MCQGGDFTR',
+                                 ['A0A7I2V4V1'],
+                                 1.52664048381647e-05, False)
+    peptide_id_list.append(peptide_id)
+
+
+
+
+
+    peptide_id = fill_peptide_id('GDLWETR',
+                                 ['DECOY PAL4E', 'DECOY PAL4H', 'DECOY PAL4D', 'DECOY PAL4F', 'DECOY PAL4A',
+                                  'DECOY A0A0H2UH3', 'DECOY PAL4C', 'DECOY PAL4G', 'DECOY PPIA'],
+                                 0.600474502860762, True)
+    peptide_id_list.append(peptide_id)
+
+    # note there were 2 decoy peptide with this unmodified sequence, but there was
+    # only 1 target peptide counterpart, I choose the one whose modified sequence
+    # is the pseudo reverse of each other
+    peptide_id = fill_peptide_id('TFDGGQCMFGPIIK',
+                                 ['DECOY PAL4E', 'DECOY PAL4H', 'DECOY PAL4D', 'DECOY PAL4F', 'DECOY PAL4A',
+                                  'DECOY A0A0H2UH3', 'DECOY PAL4C', 'DECOY PPIA', 'DECOY C9J5S7',
+                                  'F8WE65'],
+                                 0.68715324112149, True)
+    peptide_id_list.append(peptide_id)
+    peptide_id = fill_peptide_id('ATCIFFQSGNTNPGANAMSLIGSGTHR',
+                                 ['DECOY PAL4C', 'DECOY PAL4G', 'DECOY PPIA', 'DECOY C9J5S7', 'DECOY F8WE65',
+                                  'DECOY A0A7I2V4V1'],
+                                 0.536328078844151, True)
+    peptide_id_list.append(peptide_id)
+    # same situation as TFDGGQCMFGPIIK
+    peptide_id = fill_peptide_id('ATCIFFQSGNTNPGANAMSLIGPGTHR',
+                                 ['DECOY PPIA', 'DECOY C9J5S7', 'DECOY F8WE65',
+                                  'A0A7I2V4V1'],
+                                 0.407953386259084, True)
+    peptide_id_list.append(peptide_id)
+
+    # no decoy peptide
+    # peptide_id = fill_peptide_id('ELQGCDAITI',
+    #                              ['PPIA'],
+    #                              0.000330313269055314, False)
+    # peptide_id_list.append(peptide_id)
+
+    # same
+    peptide_id = fill_peptide_id('EMAEVINMGEK',
+                                 ['DECOY PPIA'],
+                                 0.315037722210494, True)
+    peptide_id_list.append(peptide_id)
+    peptide_id = fill_peptide_id('LIFNEDEFR',
+                                 ['DECOY PPIA', 'DECOY A0A7I2V4V1', 'DECOY C9J5S7', 'DECOY F8WE65'],
+                                 0.060715732750426, True)
+    peptide_id_list.append(peptide_id)
+    peptide_id = fill_peptide_id('PVKDAFLEFSVR',
+                                 ['DECOY PPIA', 'DECOY A0A7I2V5J5', 'DECOY A0A7I2V4V1', 'DECOY C9J5S7',
+                                  'DECOY F8WE65'],
+                                 0.623341508906059, True)
+    peptide_id_list.append(peptide_id)
+    peptide_id = fill_peptide_id('DAFLEFSVR',
+                                 ['DECOY PPIA', 'DECOY A0A7I2V4V1', 'DECOY C9J5S7',
+                                  'DECOY F8WE65'],
+                                 0.691996399481143, True)
+    peptide_id_list.append(peptide_id)
+    peptide_id = fill_peptide_id('GLPEGDVAIDFFVTPNVK',
+                                 ['DECOY PPIA', 'DECOY A0A7I2V5J5', 'DECOY A0A7P0S768', 'DECOY C9J5S7',
+                                  'DECOY E5RIZ5', 'DECOY F8WE65'],
+                                 0.662020173704249, True)
+    peptide_id_list.append(peptide_id)
+    peptide_id = fill_peptide_id('TFDGGQCK',
+                                 ['DECOY A0A7I2V4V1'],
+                                 0.191864127940947, True)
+    peptide_id_list.append(peptide_id)
+    peptide_id = fill_peptide_id('TFDGGQCMK',
+                                 ['DECOY A0A7I2V4V1'],
+                                 0.488750833547983, True)
+    peptide_id_list.append(peptide_id)
+
+    return peptide_id_list
 
 
 def make_protein_hit_list(protein_id, target_protein_accession_list,
@@ -151,4 +226,4 @@ def make_peptide_evidence(peptide_hit, protein_accession):
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1])
